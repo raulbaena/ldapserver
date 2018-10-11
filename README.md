61127c8376afa582102d63c1124a13652e5977af
isx53320159 <isx53320159@i12.informatica.escoladeltreball.org>
2018-10-11 09:27:50 +0200

    Proba

---
+++ README.md
@@ -0,0 +1,6 @@
+#ldapserver
+# edt asix 2018 2019
+
+Reporsitori amb les imatges de ldapserver
+ASIX M06
+PIM PAM TRUCU TRUCU
---
+++ ldapserver:18base/startup.sh
@@ -0,0 +1,8 @@
+# Version: 0.0.1
+# @edt M06 2018-2019
+# Crear i engegar slapd amb edt.org
+#-----------------------------------
+/opt/docker/install.sh && echo "Install OK"
+/sbin/slapd -d0 && echo "slapd OK"
+
+
